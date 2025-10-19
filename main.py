bddaad1de883f5593634945e9781cdd4fa37e351
btnekd-ux <btnekd@gmail.com>
2025-10-19 21:11:27 +0600

    bjb

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from fastapi import FastAPI
 
-import uvicorn 
+import uvicorn  # pyright: ignore[reportMissingImports]
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
     return {"message": "Hello World!"}
+
+
+
+
 
